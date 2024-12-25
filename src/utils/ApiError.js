ce3e2ea35b657871624075e4ae9713f2859939c1
Firoz <firozahmed709p@gmail.com>
2024-12-25 10:11:51 +0530

    middlewares

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,18 +3,18 @@ class ApiError extends Error{
     statusCode,
     message = "Internal server error",
     errors = [],
-    statck = ""
+    stack = ""
   ){
     super(message);
     this.statusCode = statusCode;
     this.errors = errors;
-    this.statck = statck;
+    this.stack = stack;
     this.data = null;
     this.success = false;
     this.message = message;
 
-    if(statck){
-      this.statck = statck;
+    if(stack){
+      this.stack = stack;
     }else{
       Error.captureStackTrace(this, this.constructor);
     }
